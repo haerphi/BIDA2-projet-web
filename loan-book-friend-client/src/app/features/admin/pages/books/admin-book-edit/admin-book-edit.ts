5d19cb88ef7da818e1f399831f0c9e988ca4584b
haerphi <haerphi@gmail.com>
2025-11-25 14:11:27 +0100

    [Angular-Prettier]

--- loan-book-friend-client/src/app/features/admin/pages/books/admin-book-edit/admin-book-edit.ts
+++ loan-book-friend-client/src/app/features/admin/pages/books/admin-book-edit/admin-book-edit.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-book-edit',
-  imports: [],
-  templateUrl: './admin-book-edit.html',
-  styleUrl: './admin-book-edit.scss'
+    selector: 'app-admin-book-edit',
+    imports: [],
+    templateUrl: './admin-book-edit.html',
+    styleUrl: './admin-book-edit.scss',
 })
-export class AdminBookEdit {
-
-}
+export class AdminBookEdit {}
